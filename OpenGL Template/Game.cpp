0a83beb21eaa2dca9c78b7de55f6b9c73a432a63
James <89285665+ji8sw@users.noreply.github.com>
2024-07-11 04:19:26 +1000

    Getting Started - Coordinate Systems

--- OpenGL Template/Game.cpp
+++ OpenGL Template/Game.cpp
@@ -3,9 +3,6 @@
 #include "Helper.h"
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb_image.h"
-#include <glm/glm.hpp>
-#include <glm/gtc/matrix_transform.hpp>
-#include <glm/gtc/type_ptr.hpp>
 
 GLFWwindow* Window = NULL;
 unsigned int VBO, VAO, EBO;
@@ -38,6 +35,7 @@ bool MakeWindow(int Width = 1920, int Height = 1080, std::string Title = "Minecr
 	glewInit();
 	glViewport(0, 0, Width, Height);
 	glfwSetFramebufferSizeCallback(Window, WindowSizeChanged);
+	glEnable(GL_DEPTH_TEST);
 	return true;
 }
 
@@ -45,23 +43,19 @@ bool CreateBuffers()
 {
 	glGenVertexArrays(1, &VAO);
 	glGenBuffers(1, &VBO);
-	glGenBuffers(1, &EBO);
 
 	glBindVertexArray(VAO);
 
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(RectangleVertices), RectangleVertices, GL_STATIC_DRAW);
 
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(RectangleIndices), RectangleIndices, GL_STATIC_DRAW);
-
 	// position attribute
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
-	// color attribute
-	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
-	glEnableVertexAttribArray(1);
 	// texture coord attribute
+	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
+	glEnableVertexAttribArray(1);
+	// colour coord attribute
 	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
 	glEnableVertexAttribArray(2);
 
@@ -157,26 +151,45 @@ int main()
 	while (!glfwWindowShouldClose(Window))
 	{
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-		glClear(GL_COLOR_BUFFER_BIT);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 		HandleInput();
-
-		glUseProgram(ShaderProgram);
-
-		glm::mat4 Transformation = glm::mat4(1.0f);
-		Transformation = glm::translate(Transformation, glm::vec3(0.5f, -0.5f, 0.0f));
-		Transformation = glm::rotate(Transformation, (float)glfwGetTime(), glm::vec3(0.0, 0.0, 1.0));
-
-		unsigned int TransformLocation = glGetUniformLocation(ShaderProgram, "Transform");
-		glUniformMatrix4fv(TransformLocation, 1, GL_FALSE, glm::value_ptr(Transformation));
 
 		glActiveTexture(GL_TEXTURE0);
 		glBindTexture(GL_TEXTURE_2D, Texture);
 		glActiveTexture(GL_TEXTURE1);
 		glBindTexture(GL_TEXTURE_2D, FaceTexture);
 
+		glUseProgram(ShaderProgram);
+
+		glm::mat4 Model      = glm::mat4(1.0f);
+		glm::mat4 View		 = glm::mat4(1.0f);
+		glm::mat4 Projection = glm::mat4(1.0f);
+		Model = glm::rotate(Model, (float)glfwGetTime(), glm::vec3(0.5f, 1.0f, 0.0f));
+		View = glm::translate(View, glm::vec3(0.0f, 0.0f, -3.0f));
+		Projection = glm::perspective(glm::radians(45.0f), 1920.0f / 1080.0f, 0.1f, 100.0f);
+
+		int ModelLocation = glGetUniformLocation(ShaderProgram, "Model");
+		int ViewLocation = glGetUniformLocation(ShaderProgram, "View");
+		glUniformMatrix4fv(ModelLocation, 1, GL_FALSE, glm::value_ptr(Model));
+		glUniformMatrix4fv(ViewLocation, 1, GL_FALSE, &View[0][0]);
+		int ProjectionLocation = glGetUniformLocation(ShaderProgram, "Projection");
+		glUniformMatrix4fv(ProjectionLocation, 1, GL_FALSE, glm::value_ptr(Projection));
+
 		glBindVertexArray(VAO);
 		//glDrawArrays(GL_TRIANGLES, 0, 3);
-		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
+		glBindVertexArray(VAO);
+		for (unsigned int i = 0; i < 10; i++)
+		{
+			glm::mat4 Model = glm::mat4(1.0f);
+			Model = glm::translate(Model, CubePositions[i]);
+			float angle = 20.0f * i;
+			Model = glm::rotate(Model, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
+			int ModelLocation = glGetUniformLocation(ShaderProgram, "Model");
+			glUniformMatrix4fv(ModelLocation, 1, GL_FALSE, glm::value_ptr(Model));
+
+			glDrawArrays(GL_TRIANGLES, 0, 36);
+		}
 
 		glfwSwapBuffers(Window);
 		glfwPollEvents();
--- OpenGL Template/Helper.h
+++ OpenGL Template/Helper.h
@@ -6,27 +6,71 @@
 #include <iostream>
 #include <Windows.h>
 #include <format>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
 
 enum ExitCodes {
 	Success = 0,
 	Error = 1
 };
 
-float TriangleVertices[] =
-{
-	// positions         // colors
-	0.5f, -0.5f, 0.0f,  1.0f, 0.0f, 0.0f,   // bottom right
-   -0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,   // bottom left
-	0.0f,  0.5f, 0.0f,  0.0f, 0.0f, 1.0f    // top 
-};
-
 float RectangleVertices[] =
 {
-	// positions          // colors           // texture coords
-	 0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f, // top right
-	 0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f, // bottom right
-	-0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f, // bottom left
-	-0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f  // top left 
+    -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+     0.5f, -0.5f, -0.5f,  1.0f, 0.0f,
+     0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+     0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+    -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+    -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+
+    -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+     0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+     0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+     0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+    -0.5f,  0.5f,  0.5f,  0.0f, 1.0f,
+    -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+
+    -0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+    -0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+    -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+    -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+    -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+    -0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+     0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+     0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+     0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+     0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+     0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+     0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+    -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+     0.5f, -0.5f, -0.5f,  1.0f, 1.0f,
+     0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+     0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+    -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+    -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+
+    -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+     0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+     0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+     0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+    -0.5f,  0.5f,  0.5f,  0.0f, 0.0f,
+    -0.5f,  0.5f, -0.5f,  0.0f, 1.0f
+};
+
+glm::vec3 CubePositions[] = {
+    glm::vec3(0.0f,  0.0f,  0.0f),
+    glm::vec3(2.0f,  5.0f, -15.0f),
+    glm::vec3(-1.5f, -2.2f, -2.5f),
+    glm::vec3(-3.8f, -2.0f, -12.3f),
+    glm::vec3(2.4f, -0.4f, -3.5f),
+    glm::vec3(-1.7f,  3.0f, -7.5f),
+    glm::vec3(1.3f, -2.0f, -2.5f),
+    glm::vec3(1.5f,  2.0f, -2.5f),
+    glm::vec3(1.5f,  0.2f, -1.5f),
+    glm::vec3(-1.3f,  1.0f, -1.5f)
 };
 
 unsigned int RectangleIndices[] =
@@ -50,15 +94,17 @@ const char* StandardVertexShaderSource =
 R"(
 #version 330 core
 layout(location = 0) in vec3 VertexPosition;
-layout(location = 2) in vec2 TextureCoordinates;
+layout(location = 1) in vec2 TextureCoordinates;
 
 out vec2 OutTextureCoordinates;
 
-uniform mat4 Transform;
+uniform mat4 Model;
+uniform mat4 View;
+uniform mat4 Projection;
 
 void main()
 {
-	gl_Position = Transform * vec4(VertexPosition, 1.0);
+	gl_Position = Projection * View * Model * vec4(VertexPosition, 1.0);
 	OutTextureCoordinates = vec2(TextureCoordinates.x, TextureCoordinates.y);
 };
 )";
@@ -68,7 +114,6 @@ R"(
 #version 330 core
 out vec4 FragColor;
 
-in vec3 OutColour;
 in vec2 OutTextureCoordinates;
 
 uniform sampler2D Texture1;
